7e4c522a72dd2f2bd8c930f74038ae6cd52080a3
hoparkgo9ma <me@ho9.me>
2018-10-24 18:06:56 +0900

    파일 추가: 초기 작업 파일 업로드

--- tables/initialize.sql
+++ tables/initialize.sql
@@ -146,4 +146,5 @@ INSERT INTO user_group_acl (
 );
 
 INSERT INTO user_acl_list_tb values(1, "owner");
-INSERT INTO static_page_tb values("frontpage", "")
+INSERT INTO static_page_tb values("frontpage", "");
+INSERT INTO static_page_tb values("adminpage", "<h1>환영합니다!</h1><p>이곳은 관리자 페이지입니다. 관리자 권한을 받은 acl 그룹의 유저만 접근할 수 있으며, 메인 페이지의 각종 작업들을 수행할 수 있습니다.</p>")
