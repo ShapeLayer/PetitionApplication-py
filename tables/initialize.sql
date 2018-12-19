91f6f7c92d3d2441ab2adc22b176934f37fe4e1b
hoparkgo9ma <me@ho9.me>
2018-12-20 07:26:41 +0900

    대문 기본 설정 적용 / 하단 저작권 표기 간소화

--- tables/initialize.sql
+++ tables/initialize.sql
@@ -146,6 +146,7 @@ INSERT INTO user_group_acl (
 );
 
 INSERT INTO user_acl_list_tb values(1, "owner");
-INSERT INTO static_page_tb (page_name, title, editor, editdate, content) values("frontpage", "%_appname_%: 대문", "System", "", "");
+INSERT INTO static_page_tb (page_name, title, editor, editdate, content) values("frontpage", "%_appname_%: 대문", "System", "", "<div class='container'><h1>BLEND IT<br>WITH<BR>YOUR WAY</h1><h2>당신의 힘으로 바꿔보세요. %_appname_% 청원페이지</h2><a href='/login' class='btn btn-primary'>시작하기</a><a href='/a' class='btn btn-link'>청원 둘러보기</a><a href='/a/write' class='btn btn-link'>청원 생성하기</a></div>
+");
 INSERT INTO static_page_tb (page_name, title, editor, editdate, content) values("adminpage", "관리자: 대문", "System", "", "<h1>환영합니다!</h1><p>이곳은 관리자 페이지입니다. 관리자 권한을 받은 acl 그룹의 유저만 접근할 수 있으며, 메인 페이지의 각종 작업들을 수행할 수 있습니다.</p>");
 INSERT INTO static_page_tb (page_name, title, editor, editdate, content) values("notice", "공지", "System", "", "아직 공지가 없습니다.");
